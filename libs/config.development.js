60bfbf7481a951522867998f5cc4a1ddc1c82784
Sofia Lunkes <sofia.big@live.com>
2017-01-14 02:43:48 -0200

    adicionando configuração para logs

--- libs/config.development.js
+++ libs/config.development.js
@@ -1,8 +1,18 @@
+import logger from "./logger.js";
+
 module.exports = {
   database: "ntask",
   username: "root",
   password: "root",
-  params:{dialect : "mysql", define: {underscored:true}},
+  params: {
+    dialect : "mysql",
+    logging : (sql) => {
+      logger.info(`[${new Date()}] ${sql}`);
+    },
+    define: {
+      underscored:true
+    }
+  },
   jwtSecret : "NT@$K - AP1",
   jwtSession : {session : false}
 };
